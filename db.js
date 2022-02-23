2e44157a536bbe18a2d70b65e2a63509c1e8edca
Luiz Paulo <62365747+luizpaulo2005@users.noreply.github.com>
2022-02-23 20:56:12 +0000

    Criação do Arquivo db.js Instalação do plugin pg Conexão com o ElephantSQL

---
+++ db.js
@@ -0,0 +1,15 @@
+let connection = function(){
+  if (global.connection){
+    return global.connection.connect();
+  }
+
+  const { Pool } = require("pg");
+  const pool = new Pool({
+    connectionString: "postgres://wsqgdvbz:O7ZBzknOMInZGF77s6t0zqZ_BkvCE1XB@motty.db.elephantsql.com/wsqgdvbz"
+  });
+
+  global.connection = pool
+  return pool.connect();
+}
+
+module.exports = { connect }

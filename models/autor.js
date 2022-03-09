2cb55601798fe851de111fb260ca6bfc8ce3e66e
Luiz Paulo <62365747+luizpaulo2005@users.noreply.github.com>
2022-03-09 19:37:39 +0000

    Adicionado comandos CRUD

--- models/autor.js
+++ models/autor.js
@@ -3,7 +3,7 @@ const db = require("../db")
 class Autor {
   static async inserir(){
     const connect = await db.connect();
-    return await connect.query("");
+    return await connect.query("insert into autores");
   }
   static async selecionar(){
     const connect = await db.connect();
@@ -11,11 +11,11 @@ class Autor {
   }
   static async atualizar(){
     const connect = await db.connect();
-    return await connect.query("");
+    return await connect.query("update autores");
   }
   static async deletar(){
     const connect = await db.connect();
-    return await connect.query("");
+    return await connect.query("delete from autores");
   }
   }
 
